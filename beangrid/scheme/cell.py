cec3014be1a52b9a284de1b304aa40c5a59e58eb
Fang-Pen Lin <hello@fangpenlin.com>
2025-07-25 18:49:28 -0700

    Add more models

--- beangrid/scheme/cell.py
+++ beangrid/scheme/cell.py
@@ -1,8 +1,16 @@
 from pydantic import BaseModel
 
 
-
 class Cell(BaseModel):
     id: str
     value: str | None
     formula: str | None
+
+
+class Sheet(BaseModel):
+    name: str
+    cells: list[Cell]
+
+
+class Workbook(BaseModel):
+    sheets: list[Sheet]
